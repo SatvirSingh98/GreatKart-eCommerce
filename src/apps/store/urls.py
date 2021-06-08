dd43e0cbb2d947f82ef3b31af7f68b89adc7cc1e
satvir <satvirsingh.juc@gmail.com>
2021-06-08 17:22:35 +0530

    display products in store page

--- src/apps/store/urls.py
+++ src/apps/store/urls.py
@@ -6,4 +6,5 @@ app_name = 'store'
 
 urlpatterns = [
     path('', store_view, name='store-home'),
+    path('<slug:category_slug>/', store_view, name='products-by-category'),
 ]
--- src/apps/store/views.py
+++ src/apps/store/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from apps.category.models import Category
 
 from .models import Product
 
 
-def store_view(request):
-    products = Product.objects.all()
+def store_view(request, category_slug=None):
+    if category_slug is not None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=category)
+    else:
+        products = Product.objects.all()
     return render(request, 'store/store.html', {'products': products})
