ad932b6802fc44f96ce3aa5aceaee20e6e8da398
satvir <satvirsingh.juc@gmail.com>
2021-06-08 17:44:06 +0530

    Cart and CartItems models

--- src/apps/cart/models.py
+++ src/apps/cart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from apps.store.models import Product
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product.name
