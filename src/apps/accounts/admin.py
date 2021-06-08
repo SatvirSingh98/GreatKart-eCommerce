aa4a74cb729ff18476133f81848e3a0b5512f8a4
satvir <satvirsingh.juc@gmail.com>
2021-06-08 17:02:22 +0530

    making password field readonly

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Account
 
 
 @admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
+class AccountAdmin(UserAdmin):
     '''Admin View for Account'''
 
-    list_display = ('__str__', 'first_name', 'last_name', 'email')
+    list_display = ('__str__', 'first_name', 'last_name', 'email', 'date_joined', 'last_login', 'is_active')
+    readonly_fields = ('date_joined', 'last_login')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
