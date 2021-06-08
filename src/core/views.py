753ab6d0c06508c0b012e2f0b4b050b96aa9e2a8
satvir <satvirsingh.juc@gmail.com>
2021-06-08 17:12:52 +0530

    Display products in homepage

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from apps.store.models import Product
+
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all().filter(is_available=True)
+    return render(request, 'index.html', {'products': products})
