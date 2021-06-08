b7f94e02856bfc13d700536da775ff35d97d016d
satvir <satvirsingh.juc@gmail.com>
2021-06-08 18:43:43 +0530

    Check If the Product Added to the Cart

--- src/apps/store/views.py
+++ src/apps/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
+from apps.cart.models import CartItem
+from apps.cart.views import _cart_id
 from apps.category.models import Category
 
 from .models import Product
@@ -16,4 +18,5 @@ def store_view(request, category_slug=None):
 
 def product_detail_view(request, category_slug=None, product_slug=None):
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
-    return render(request, 'store/product-detail.html', {'product': product})
+    in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=product).exists()
+    return render(request, 'store/product-detail.html', {'product': product, 'in_cart': in_cart})
