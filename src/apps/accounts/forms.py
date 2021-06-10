5da3b323d0bdbe94aa46ccdbbb4970165b4ae890
satvir <satvirsingh.juc@gmail.com>
2021-06-10 13:45:00 +0530

    Implementing Model Forms and Editing __init__ Method

--- src/apps/accounts/forms.py
+++ src/apps/accounts/forms.py
@@ -4,10 +4,22 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
-    """Form definition for Registration."""
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields.get('first_name').widget.attrs['placeholder'] = 'first name'
+        self.fields.get('last_name').widget.attrs['placeholder'] = 'last name'
+        self.fields.get('email').widget.attrs['placeholder'] = 'provide working email id'
+        self.fields.get('phone_number').widget.attrs['placeholder'] = 'phone number'
+        for field in self.fields:
+            self.fields.get(field).widget.attrs['class'] = 'form-control'
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+    }))
 
     class Meta:
-        """Meta definition for Registrationform."""
-
         model = Account
-        fields = ('first_name', 'last_name', 'email', 'phone_number', 'password',)
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
